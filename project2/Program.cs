b81228d56af2ec02be9f3ccd02f6abd3943afa02
anapalihovici <ana.palihovici@gmail.com>
2015-07-06 18:47:00 +0300

    probl9

---
+++ project2/Program.cs
@@ -0,0 +1,65 @@
+﻿//problema  9
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project2
+{
+    
+    struct employee {public string first_name; public string family_name;};
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            employee[] arr = new employee[100];
+           // int[] arrMyValues = new int[] { 1, 2, 3 };
+            List<int> employee_nr = new List<int>();
+            Random rnd = new Random();
+            int i = 0, r;
+
+            // Read the text file line by line and store from each line the first name and the family name
+            string[] lines = System.IO.File.ReadAllLines(@"C:\Users\ana.palihovici\Documents\Visual Studio 2013\Projects\project2\project2\employees.txt");
+
+            System.Console.WriteLine("Employees:");
+            foreach (string line in lines)
+            {
+                i++;
+                System.Console.WriteLine("Number of Employees: " + i);
+                // Use a tab to indent each line of the file.
+                Console.WriteLine("\t" + line);
+                string[] words;
+                words = line.Split(' ');
+                arr[i].first_name = words[0];
+                arr[i].family_name = words[1];
+                employee_nr.Add(i);
+                
+            }
+           
+            //after we have read all employees we assign each of them a Secret Santa
+
+            for (int k = 1; k <= i;k++ )
+            {
+                System.Console.WriteLine("k: " + k + "i: "+ i);
+               
+                do
+                { r = rnd.Next(employee_nr.Count); }
+                while (r == k || !(employee_nr.Contains(r)));
+                Console.WriteLine("r=" + r + "k=" + k);
+                Console.WriteLine("\t" + arr[k].first_name + " " + arr[k].family_name + " has been assigned: " + arr[r].first_name + " " + arr[r].family_name+"     ");
+                //Console.WriteLine("\t" + arr[r].first_name + " " + arr[r].family_name);
+              // employee_nr.Remove(r);
+               
+            }
+                
+
+                // Keep the console window open in debug mode.
+                Console.WriteLine("Press any key to exit.");
+                System.Console.ReadKey();
+        }
+
+    }
+}
