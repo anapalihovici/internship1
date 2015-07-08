d3b0862c3d118962bcb957c96648f15fd06b65a1
anapalihovici <ana.palihovici@gmail.com>
2015-07-08 17:38:56 +0300

    changes

--- project1.cs/Properties/AssemblyInfo.cs
+++ project1.cs/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("project4")]
+[assembly: AssemblyTitle("project1.cs")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("project4")]
+[assembly: AssemblyProduct("project1.cs")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("9302430f-a5bd-40d9-99a2-8f4f0b7485ee")]
+[assembly: Guid("433884a7-7d52-4cac-88cd-d47e6393a16d")]
 
 // Version information for an assembly consists of the following four values:
 //
